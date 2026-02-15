dac431299c68053d16281232a573afb1c6b4a6f4
Gabriello11 <>
2026-02-15 19:29:33 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,22 @@
+grocery_items = "milk cheese bread"
+# String Slice 
+dairy1 = grocery_items[0:4]
+dairy2 = grocery_items[6:11]
+barkery1 = grocery_items[12:] 
+
+print(grocery_items)
+
+#string concatenation
+dairy1 = "milk"
+dairy2 = "cheese"
+barkery1 = "bread"
+aisle = 5
+
+part1 = "mi"
+part2 = "lk"
+new_word = part1 + part2
+print("Concetenated word:",new_word)
+
+
+#message1 = (f"We have dairy and bakery items: {dairy1}  + {dairy2} + {barkery1} in aisle {aisle}")
+#print(message)
